44337858cbbd2cd1e3aa41f40f93838acb26ca24
DELL <DELL@DESKTOP-0MRUKOH>
2024-04-06 19:30:53 +0530

    first commit

---
+++ helloworld/src/com/harshu/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.harshu.encapsulation;
+//to demostarte encapsulation
+
+public class Encapsulation {
+	int n;   // data members
+	String s;
+	int a;
+	
+	public void show() {     // member function
+		System.out.println("Serialnumber"+ n +"\t name "+  s+"\t age" +a);
+	}
+}
+
+
+
---
+++ helloworld/src/com/harshu/encapsulation/Exec.java
@@ -0,0 +1,15 @@
+package com.harshu.encapsulation;
+
+public class Exec {
+	public static void main(String[] args) {
+		
+		Encapsulation e=new Encapsulation();
+		e.n=7;
+		e.s="harshu";
+		e.a=19;
+		
+		e.show();
+	}
+	}
+
+
